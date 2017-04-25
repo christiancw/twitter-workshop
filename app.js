5543baa454f6e50ff1bee56d88bf1e29e7aefabd
Christian Wentling <christianwentling@gmail.com>
2017-04-25 10:36:40 -0400

    with render

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require( 'express' );
+const nunJ = require('nunjucks');
 const app = express();
 
 app.get('/', function(req, res){
   console.log('GET' + req.path);
-  res.send('hello');
+  var template = nunJ.configure('views', {
+    noCache: true,
+    express: app});
+  // console.log('GET' + res.status(200));
+  res.send(template.render('index.html', locals));
 })
 
-
+var locals = {
+  title: 'An Example',
+  people: [
+      { name: 'Gandalf'},
+      { name: 'Frodo' },
+      { name: 'Hermione'}
+    ]
+};
 
 
 var server = app.listen(3000);
