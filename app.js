8e958b260748888b50ce6fd8e133564babeee47a
Christian Wentling <christianwentling@gmail.com>
2017-04-25 16:09:00 -0400

    almost final

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 const bodyParser = require('body-parser')
 const socketio = require('socket.io')
 
-// var server = app.listen(3000);
-// var io = socketio.listen(server);
+var server = app.listen(3000);
+var io = socketio.listen(server);
 
 // app.get('/', function(req, res){
 //   console.log('GET' + req.path);
@@ -16,9 +16,9 @@ app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunJ.render); // when giving html files to res.render, tell it to use nunjucks
 nunJ.configure('views', {noCache : true}); // point nunjucks to the proper directory for templates
 
-app.use('/', routes);
+app.use('/', routes(io));
 app.use(express.static('public'));
 
 
 
-var server = app.listen(3000);
+// var server = app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,31 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
+var Tweet = function(name, content){
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets, showForm : true } );
-});
+}
 
-router.get('/users/:name', function(req, res){
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list, showForm : true, value: name } );
-});
+module.exports = function(io){
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets: tweets, showForm : true } );
+  });
 
-router.post('/tweets', function(req, res) {
-  console.log(req.body)
-  var name = req.body.name;
-  // console.log(req.body);
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
-});
+  router.get('/users/:name', function(req, res){
+    var name = req.params.name;
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { tweets: list, showForm : true, value: name } );
+  });
 
-// router.get('/stylesheets/style.css', function(req, res) {
-//   res.sendFile('stylesheets/style.css', {root: './public/'},
-//   function(err){
-//     if(err) throw err;
-//     console.log('Sent file');
-//   })
-// })
+  router.post('/tweets', function(req, res) {
+    console.log(req.body)
+    var name = req.body.name;
+    // console.log(req.body);
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    io.sockets.emit('newTweet', new Tweet(name, text));
+    res.redirect('/');
+  });
 
-module.exports = router;
+  return router;
+}
